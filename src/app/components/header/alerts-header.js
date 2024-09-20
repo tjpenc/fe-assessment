a79832167f35b9389e400fdd4af4e0ebeebbb61b
Thomas Pencarinha <tjpencarinha@gmail.com>
2024-09-20 14:26:09 -0400

    header mvp

--- src/app/components/header/alerts-header.js
+++ src/app/components/header/alerts-header.js
@@ -1,7 +1,19 @@
+import styles from "./alerts-header.module.css";
+
 const AlertsHeader = () => {
     return (
         <>
-          <div>Alerts Header</div>
+          <div className={styles["wait-times-container"]}>
+            View All Er Wait Times
+          </div>
+          <div className={styles["alerts-container"]}>
+            <button className={`${styles["btn-general"]} ${styles["appointment-button"]}`}>
+                Make an Appointment
+            </button>
+            <button className={`${styles["btn-general"]} ${styles["alerts-button"]}`}>
+                Alerts
+            </button>
+          </div>
         </>
     )
   };
--- src/app/components/header/nav-bar.js
+++ src/app/components/header/nav-bar.js
@@ -4,10 +4,21 @@ const NavBar = () => {
     return (
         <>
           <div className={styles["logo-container"]}>
-
+            Logo
           </div>
           <div className={styles["links-container"]}>
-
+            <a href='#'>Home</a>
+            <a href='#'>My Health Resources</a>
+            <a href='#'>Specialties</a>
+            <a href='#'>Locations</a>
+            <a href='#'>Patients & Visitors</a>
+            <a href='#'>Careers</a>
+            <a href='#'>About Us</a>
+            <a href='#'>Find A Doctor</a>
+            <a href='#'>Manage Your Account</a>
+            <a href='#'>MyHealthONE</a>
+            <a href='#'>...</a>
+            <input placeholder="Search"/>
           </div>
         </>
     )
