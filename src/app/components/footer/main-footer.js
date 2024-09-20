a2ece1c3db3918fdf417000e6bcca7cee4668126
Thomas Pencarinha <tjpencarinha@gmail.com>
2024-09-20 15:12:58 -0400

    footer styling mvp

--- src/app/components/footer/main-footer.js
+++ src/app/components/footer/main-footer.js
@@ -1,7 +1,34 @@
+import styles from './main-footer.module.css'
+
 const MainFooter = () => {
     return (
         <>
-          <div>Main Footer</div>
+          <div className={styles["logo-container"]}>
+            <div className={styles["logo"]}>Image</div>
+            <div className={styles["address"]}>
+                <p>HCA Houston Healthcare</p>
+                <p>3737 Buffalo Speedway</p>
+                <p>Suite 1400</p>
+                <p>Houston, TX 77098</p>
+            </div>
+          </div>
+          <div className={styles["about-container"]}>
+            <p>About Us</p>
+            <a href="#">About HCA Houston</a>
+            <a href="#">Phone Directory</a>
+            <a href="#">Maps & Directions</a>
+            <a href="#">Media Center</a>
+            <a href="#">Contact Us</a>
+          </div>
+          <div className={styles["socials-container"]}>
+            <p>Follow Us</p>
+            <div className={styles["socials"]}>
+                <p>Logo</p>
+                <p>Logo</p>
+                <p>Logo</p>
+                <p>Logo</p>
+            </div>
+          </div>
         </>
     )
   };
--- src/app/components/footer/sub-footer.js
+++ src/app/components/footer/sub-footer.js
@@ -1,7 +1,25 @@
+import styles from './sub-footer.module.css';
+
 const SubFooter = () => {
     return (
         <>
-          <div>Sub Footer</div>
+          <div className={styles["copywright-container"]}>Copywright 1999-2021 <a href="#">&copy;HCA Inc:</a> All rights reserved</div>
+          <div className={styles["links-container"]}>
+            <div>
+                <a href="#">Notice of Privacy Practice</a> |
+                <a href="#"> Terms & Conditions</a> |
+                <a href="#"> Notice at Collection</a> |
+                <a href="#"> Privacy Policy</a> |
+                <a href="#"> Do Not Sell My Personal Information</a>
+            </div>
+            <div>
+                <a href="#">Social Media Policy</a> |
+                <a href="#"> Acceptable Use Policy</a> |
+                <a href="#"> HCA Nondiscrimination Notice</a> |
+                <a href="#"> Accessability</a> |
+                <a href="#"> Responsible Disclosure</a>
+            </div>
+          </div>
         </>
     )
   };
