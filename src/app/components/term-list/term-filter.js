9e7c7176b3a41f7959a55dd60c3e4510dcf53cc4
Thomas Pencarinha <tjpencarinha@gmail.com>
2024-09-20 12:57:28 -0400

    created term list and filtering

--- src/app/components/term-list/term-filter.js
+++ src/app/components/term-list/term-filter.js
@@ -1,7 +1,27 @@
-const TermFilter = () => {
+import styles from "./term-filter.module.css"
+
+const TermFilter = ({ activeLetters, handleFilterClick }) => {
+    let letters = []
+    for (let i = 0; i < 26; i++) {
+        let letter = String.fromCharCode(65 + i);
+        letters.push(letter);
+        if (i == 25) {
+            letters.push('#');
+        }
+    }
+
     return (
         <>
-          <div>Term Filter</div>
+          <div className={styles["filter-header"]}>Treatments, Services and Specialties</div>
+          <div className={styles["letter-container"]}>
+            {letters.map((letter, index) => {
+                if (activeLetters.includes(letter)) {
+                    return <p key={index} className={styles["active-letter"]} onClick={() => handleFilterClick(letter)}>{letter}</p>
+                } else {
+                    return <p key={index} className={styles["inactive-letter"]}>{letter}</p>
+                }
+            })}
+          </div>
         </>
     )
   };
